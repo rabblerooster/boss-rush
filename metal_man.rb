e85961c10e607000b5893f42b39a2e08ee86d35b
Rob Rypka <rabblerooster@gmail.com>
2021-03-14 18:19:56 -0500

    Metal Man updates, token fix.

--- metal_man.rb
+++ metal_man.rb
@@ -2,7 +2,7 @@ require 'squib'
 
 data = Squib.csv file: 'data/metal_man.csv'
 
-locations = ['InTop', 'InMiddle', 'InBottom', 'OutTop', 'OutMiddle', 'OutBottom']
+locations = ['TopLeft', 'TopMiddle', 'TopRight', 'BottomLeft', 'BottomMiddle', 'BottomRight']
 
 Squib::Deck.new(cards: data.nrows) do
   use_layout file: 'layouts/metal_man.yml'
@@ -10,15 +10,19 @@ Squib::Deck.new(cards: data.nrows) do
   background color: '#390016'
 
   text str: data['Action'].map { |a| a.upcase }, layout: :Action
-  text str: data['Hint'], layout: :Hint
+  text str: data['Damage'].map { |a| "Damage " + a.to_s }, layout: :Damage
+  text str: data['Delay'].map { |a| "Delay " + a.to_s }, layout: :Delay
 
   locations.each { |loc|
     text str: data[loc], layout: loc
   }
 
-  # png file: data['Icon'].map { |a| "icons/#{a}" } , layout: :Icon
+  line layout: :CenterLine
+  line layout: :BottomLine
+  line layout: :LeftLine
+  line layout: :RightLine
 
   save format: :png
   save_png prefix: 'metal_'
-  save_sheet prefix: 'sheet_metal_', columns: 5, rows: 5
+  save_sheet prefix: 'sheet_metal_', columns: 5, rows: 2
 end
